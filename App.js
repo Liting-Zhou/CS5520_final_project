7feaf275304b69e17e40f66ee79074ad6769bfdf
Liting-Zhou <143493320+Liting-Zhou@users.noreply.github.com>
2024-08-01 17:42:01 -0700

    build the basic 5 screens and put in bottom navigation

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
+import Rates from "./screens/Rates";
+import Conversion from "./screens/Conversion";
+import Assets from "./screens/Assets";
+import LocationFinder from "./screens/LocationFinder";
+import Profile from "./screens/Profile";
+
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Rates"
+        screenOptions={{
+          tabBarStyle: {},
+        }}
+      >
+        <Tab.Screen name="Rates" component={Rates} />
+        <Tab.Screen name="Assets" component={Assets} />
+        <Tab.Screen name="Conversion" component={Conversion} />
+        <Tab.Screen name="Finder" component={LocationFinder} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
