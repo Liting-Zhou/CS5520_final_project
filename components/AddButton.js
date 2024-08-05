ec01c50e390e1cf450d9f751ac23178c038d9cd6
oiat11 <fu.qia@northeastern.edu>
2024-08-05 15:17:34 -0700

    added feedback to add button

--- components/AddButton.js
+++ components/AddButton.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Pressable } from "react-native";
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { colors } from "../helpers/Constants";
 
 export default function AddButton({ onPress }) {
   return (
-    <Pressable onPress={onPress}>
-      <Ionicons name="add" size={24} color="black" style={styles.icon} />
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [
+        styles.icon,
+        { opacity: pressed ? 0.5 : 1 }
+      ]}
+      android_ripple={{ color: colors.lightGray }}
+    >
+      <Ionicons name="add" size={24} color="black" />
     </Pressable>
   );
 }
