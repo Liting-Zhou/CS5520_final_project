e00b53e5e85b81be24b0095cf6467646cfea5b5b
oiat11 <fu.qia@northeastern.edu>
2024-08-05 14:13:37 -0700

    styled the container of transaction detail

--- components/TransactionDetail.js
+++ components/TransactionDetail.js
@@ -5,11 +5,15 @@ import { colors, textSizes } from '../helpers/Constants';
 export default function TransactionDetail({ transaction }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.description}>{transaction.description}</Text>
-      <Text style={styles.location}>{transaction.location}</Text>
-      <Text style={styles.date}>{new Date(transaction.date).toLocaleDateString()}</Text>
-      <Text style={styles.currency}>From:  {transaction.fromCurrency} {transaction.fromAmount}</Text>
-      <Text style={styles.currency}>To:  {transaction.toCurrency} {transaction.toAmount}</Text>
+      <View style={styles.leftColumn}>
+        <Text style={styles.description}>{transaction.description}</Text>
+        <Text style={styles.date}>{new Date(transaction.date).toLocaleDateString()}</Text>
+        <Text style={styles.location}>{transaction.location}</Text>
+      </View>
+      <View style={styles.rightColumn}>
+        <Text style={styles.currency}>From: {transaction.fromCurrency} {transaction.fromAmount}</Text>
+        <Text style={styles.currency}>To: {transaction.toCurrency} {transaction.toAmount}</Text>
+      </View>
     </View>
   );
 }
@@ -22,23 +26,36 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: colors.gray,
     borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  leftColumn: {
+    flex: 1,
+    paddingRight: 10,
+  },
+  rightColumn: {
+    flex: 1,
+    alignItems: 'flex-end',
+    marginTop: 35,
   },
   description: {
-    borderColor: colors.gray,
     fontSize: textSizes.large,
     fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  date: {
+    fontSize: textSizes.small,
+    color: colors.lightGray,
     marginBottom: 5,
   },
   location: {
     fontSize: textSizes.medium,
     color: colors.gray,
-  },
-  date: {
-    fontSize: textSizes.small,
-    color: colors.lightGray,
+    marginBottom: 5,
   },
   currency: {
     fontSize: textSizes.small,
     color: colors.darkGray,
+    marginBottom: 5,
   },
 });
