82e00d6ee120604b27ac047c58868866a9f98ec1
Liting-Zhou <143493320+Liting-Zhou@users.noreply.github.com>
2024-08-11 22:57:57 -0700

    addButton pressRetentionOffset

--- components/AddButton.js
+++ components/AddButton.js
@@ -9,6 +9,7 @@ export default function AddButton({ onPress }) {
       onPress={onPress}
       style={({ pressed }) => [styles.icon, { opacity: pressed ? 0.5 : 1 }]}
       android_ripple={{ color: colors.lightGray }}
+      pressRetentionOffset={{ top: 10, left: 10, right: 10, bottom: 10 }}
     >
       <Ionicons name="add" size={24} color="black" />
     </Pressable>
